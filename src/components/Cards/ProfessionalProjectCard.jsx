d6b9e930a8dc66efe150f7394bfbe77379114689
Max McKinney <maxgames101@gmail.com>
2024-02-22 14:20:55 -0600

    asd

--- src/components/Cards/ProfessionalProjectCard.jsx
+++ src/components/Cards/ProfessionalProjectCard.jsx
@@ -3,31 +3,30 @@ import Image from "next/image";
 import Pill from "@components/Pill";
 import ReadMoreButton from "@components/Buttons/ReadMoreButton";
 import { MBody, MHeading02 } from "@components/Typography";
-import useBase64Image from "@/hooks/useBase64Image";
 
-const ProfessionalProjectCard = async ({ data }) => {
+const ProfessionalProjectCard = async ({ frontmatter, slug }) => {
     return (
         <div>
             {/* Preview Image */}
             <div className="rounded-lg relative max-w-7xl h-96 border border-white/20 overflow-hidden mx-auto md:h-[650px]">
-                {/* <img className="object-cover h-full" src={data.frontmatter.thumb} alt={"Project image for portfolio item " + data.frontmatter.title} /> */}
+                <Image className="object-cover h-full" src={frontmatter.thumb} alt={"Project image for portfolio item " + frontmatter.title} />
             </div>
 
             {/* Card Content */}
             <div className="contained-content mt-12 flex flex-col gap-6 items-start">
                 {/* Pills */}
                 <div className="inline-grid grid-flow-col gap-3">
-                    <Pill type="primary" text={data.frontmatter.projectRole} />
+                    <Pill type="primary" text={frontmatter.projectRole} />
                 </div>
 
                 {/* Words */}
                 <div className="flex flex-col gap-4">
-                    <MHeading02>{data.frontmatter.title}</MHeading02>
-                    <MBody className="text-zinc-400 flex-1">{data.frontmatter.projectShortBrief}</MBody>
+                    <MHeading02>{frontmatter.title}</MHeading02>
+                    <MBody className="text-zinc-400 flex-1">{frontmatter.projectShortBrief}</MBody>
                 </div>
 
                 <div id="ProjectActions">
-                    <ReadMoreButton accent={data.frontmatter.accentColor} href={`/work/${data.slug}`}>
+                    <ReadMoreButton accent={frontmatter.accentColor} href={`/work/${slug}`}>
                         View case study
                     </ReadMoreButton>
                 </div>
