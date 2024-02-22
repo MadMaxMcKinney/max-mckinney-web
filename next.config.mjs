6b8e8d75f6a12d664f678bba88ec1b1d7c506f12
Max McKinney <maxgames101@gmail.com>
2024-02-21 20:06:25 -0600

    trying to fix vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
-// @ts-check
-import withPlaiceholder from "@plaiceholder/next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: { remotePatterns: [{ hostname: "*" }] },
 };
 
-export default withPlaiceholder(nextConfig);
+export default nextConfig;
