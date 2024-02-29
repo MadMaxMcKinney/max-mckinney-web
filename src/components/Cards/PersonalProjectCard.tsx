19430a19f4c21f598df57c3845820872746aef2f
Max McKinney <maxgames101@gmail.com>
2024-02-29 16:58:23 -0600

    design: update PersonalProjectCard for mobile friendliness with the folder version

--- src/components/Cards/PersonalProjectCard.tsx
+++ src/components/Cards/PersonalProjectCard.tsx
@@ -9,10 +9,8 @@ interface PersonalProjectCardProps {
 }
 
 export default function PersonalProjectCard(props: PersonalProjectCardProps) {
-    const cardStyles = "";
-
     return (
-        <div className="relative group">
+        <div className={`relative group ${props.isFolder ? "mt-2 sm:mt-0" : ""}`}>
             <Link
                 style={{ "--accent": props.accent } as CSSProperties}
                 className={`flex flex-col gap-6 p-6 relative rounded-3xl border border-white/15 bg-[#0c0c12] overflow-clip z-10 transition-all md:p-8 after:absolute after:inset-0 after:bg-[var(--accent)] after:opacity-0 after:transition-all hover:border-[var(--accent)] hover:after:opacity-15 active:scale-[0.97] active:border-[var(--accent)] focus-visible:border-[var(--accent)] focus-visible:outline-none ${
@@ -31,8 +29,8 @@ export default function PersonalProjectCard(props: PersonalProjectCardProps) {
 
 function FolderBackdropCard(props: { children: React.ReactNode; index: number }) {
     const cardStyles: any = {
-        1: "-z-10 scale-95 -translate-y-6 group-hover:-translate-y-8 delay-75",
-        2: "-z-20 scale-90 -translate-y-12 group-hover:-translate-y-14 delay-100",
+        1: "-z-10 scale-95 -translate-y-4 sm:-translate-y-6 group-hover:-translate-y-5 sm:group-hover:-translate-y-8 delay-75",
+        2: "-z-20 scale-90 -translate-y-8 sm:-translate-y-12 group-hover:-translate-y-9 sm:group-hover:-translate-y-14 delay-100",
     };
     return <span className={`absolute inset-0 flex flex-col gap-6 p-6 rounded-3xl border border-white/15 bg-[#0c0c12] overflow-clip transition-all ${cardStyles[props.index]}`}>{props.children}</span>;
 }
